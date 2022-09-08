74dd86c5eb9a8d877c83b5b28ee24d8bc6f4459b
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-08 10:41:28 +0530

    data showed in web

--- src/conponents/BasicTable.js
+++ src/conponents/BasicTable.js
@@ -1,15 +1,42 @@
 import React, { useMemo } from "react";
-import { usetable } from "react-table";
+import { useTable } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS } from "./column";
+import "./table.css";
 
 export const BasicTable = () => {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
 
-  const tableInstance = usetable({
+  const tableInstance = useTable({
     columns,
     data,
   });
-  return <div></div>;
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroups) => (
+          <tr {...headerGroups.getHeaderGroupProps()}>
+            {headerGroups.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
