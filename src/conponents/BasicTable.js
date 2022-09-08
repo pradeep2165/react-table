50def5cbce000507c2636a82aac3140efe42d431
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-08 17:49:48 +0530

    group inserted

--- src/conponents/BasicTable.js
+++ src/conponents/BasicTable.js
@@ -1,11 +1,11 @@
 import React, { useMemo } from "react";
 import { useTable } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
-import { COLUMNS } from "./column";
+import { GROUPED_COLUMNS } from "./column";
 import "./table.css";
 
 export const BasicTable = () => {
-  const columns = useMemo(() => COLUMNS, []);
+  const columns = useMemo(() => GROUPED_COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
 
   const { getTableProps, getTableBodyProps, headerGroups, footerGroups, rows, prepareRow } = useTable({
