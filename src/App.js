b83ac414df3976704b2b8fc4e0fd89e104af7ba5
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-09 11:42:33 +0530

    pagination added with button previousPage, nextPage with conditional disabled properties

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import { FilteringTable } from "./conponents/FilteringTable";
+// import { FilteringTable } from "./conponents/FilteringTable";
 // import { BasicTable } from "./conponents/BasicTable";
 // import { SortingTable } from "./conponents/SortingTable";
+import { PaginationTable } from "./conponents/PaginationTable";
 
 function App() {
   return (
     <div className="App">
       {/* <BasicTable /> */}
       {/* <SortingTable /> */}
-      <FilteringTable />
+      {/* <FilteringTable /> */}
+      <PaginationTable />
     </div>
   );
 }
